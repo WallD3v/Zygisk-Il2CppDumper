21e99e54ef1880483eeb990e6085c69636c8995c
WallD3v <109732229+WallD3v@users.noreply.github.com>
2024-07-28 13:04:24 +0600

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.SKITLSE.StandRise"
+#define GamePackageName "com.YonoProduction.ProjectEvolution"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
